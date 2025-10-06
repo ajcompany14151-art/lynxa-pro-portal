70a41bd70b2935af6b0f1edba9e4d19c8f4fe3e7
ajcompany14151-art <ajcompany14151@gmail.com>
2025-10-06 21:39:25 +0530

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,21 @@
+// app/layout.js
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from './providers'
+import './globals.css'
+
+export const metadata = {
+  title: 'Lynxa Pro Portal',
+  description: 'Manage your Lynxa Pro API Keys',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <html lang="en">
+          <body>{children}</body>
+        </html>
+      </ThemeProvider>
+    </ClerkProvider>
+  )
+}
